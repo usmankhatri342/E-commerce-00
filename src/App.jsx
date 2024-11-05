31eb71a5ecb298c6e3897d214af9989fe93bfec3
usmanramzan342 <usmantt.ramzan@gmail.com>
2024-11-05 20:14:53 +0500

    E-Commerce

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="border-2 flex gap-4 flex-col relative bg-[#df2f2f] w-64 h-[350px] text-green-950">
+      USMAN RAMZAN</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
